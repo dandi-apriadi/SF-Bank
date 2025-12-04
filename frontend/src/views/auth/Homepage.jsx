8a070878150be89bdedd4a0cf9e6c0ce44b86061
dandi-apriadi <dandiapeiadi22@gmail.com>
2025-12-04 09:14:51 +0800

    backup new feature

--- frontend/src/views/auth/Homepage.jsx
+++ frontend/src/views/auth/Homepage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { 
   GiCastle, 
   GiCrossedSwords, 
